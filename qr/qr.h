cca8dbba6a0e96f34e1a276e7b7d4b8ba01db644
kiliac03 <kiliac03@mi.fu-berlin.de>
2025-11-13 17:03:14 +0100

    usage

--- qr/qr.h
+++ qr/qr.h
@@ -5,8 +5,8 @@
 #include <stdio.h>
 
 qr_code *qr_create(qr_ec_level level, qr_encoding_mode mode, unsigned version);
-void qr_encode_message(qr_code *qr, const char *message);
 void qr_destroy(qr_code *qr);
+void qr_encode_message(qr_code *qr, const char *message);
 void qr_svg_print(qr_code *qr, FILE *stream);
 
 #endif // QR_QR_H
